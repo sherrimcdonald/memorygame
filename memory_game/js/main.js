b241085055dda18da50bea7f2f34dee67fd2aa59
Sherri McDonald <sherrimcdonald@Sherris-MacBook-Pro.local>
2019-03-04 12:53:08 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,23 @@
-//Create card variables
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-//Display two cards the user has flipped over
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+//Create cards array
+
+var cards = ["queen", "queen", "king", "king"];
+
+//Defines and initiates an array to pass in the values from cards object.
+var cardsInPlay = [];
+
+//Create a variable to represent the first card that the user flipped.
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push('cardOne');
+cardsInPlay.push('cardTwo');
+
+if (cardsInPlay.length === 2) {
+	if  (cardsInPlay[0] === cardsInPlay[2]){ 
+	alert("You found a match.");
+    }else {
+    alert("Sorry, try again.");
+   }
+};
+
