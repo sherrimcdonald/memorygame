00444d46e80a231ef6d33e992fa2e2f4192bce26
Sherri McDonald <sherrimcdonald@Sherris-MacBook-Pro.local>
2019-03-05 00:13:11 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,22 +2,29 @@
 //Create cards array
 
 var cards = ["queen", "queen", "king", "king"];
-
 //Defines and initiates an array to pass in the values from cards object.
 var cardsInPlay = [];
 
-//Create a variable to represent the first card that the user flipped.
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push('cardOne');
-cardsInPlay.push('cardTwo');
-
-if (cardsInPlay.length === 2) {
-	if  (cardsInPlay[0] === cardsInPlay[2]){ 
-	alert("You found a match.");
+//Function to check if two cards match when selected 
+var checkForMatch = function (){
+    if  (cardsInPlay[0] === cardsInPlay[1]) { 
+	alert("You found a match!");
     }else {
     alert("Sorry, try again.");
    }
-};
+ }
+
+//Function that fires flipping of the cards from back side to front.
+var flipCard = function(cardID) {
+	console.log("User flipped " + cards[cardID])
+	cardsInPlay.push(cards[cardID]);  
+	if (cardsInPlay.length === 2) {
+	checkForMatch();  
+	  
+  }
+}
+
+flipCard(0);
+flipCard(2);
+
 
